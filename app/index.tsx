9faded0b98099e03edff93cb1bdf70cb07071dbc
vihanga nethusara <vihanganethusara00@gmail.com>
2025-06-26 21:55:42 +0530

    chore: onborad page images

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
+import { Image } from "expo-image";
 import { Text, View } from "react-native";
 import '../global.css';
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Image
+        source={'../assets/images/onboarding/girl-reading-news-paper.png'}
+        alt="Girl Sitting on a bech reading newspaper"
+        className="w-[400px] h-1/2"
+      />
+      <Text>
+        Hello world
       </Text>
     </View>
   );
